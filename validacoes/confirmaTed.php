09961326dd259271307e2777c072c2d0ac9d3a59
MatheusBrandao525 <mafe123silva@gmail.com>
2022-04-24 07:14:51 -0400

    alterações 24/04/22

--- validacoes/confirmaTed.php
+++ validacoes/confirmaTed.php
@@ -6,17 +6,41 @@
     $agencia = $_POST['txtagencia'];
     $valorTed = $_POST['valorTed'];
     $numeroConta = $_POST['numeroconta'];
-
-/*     
-    echo $selectConta;
-    echo '<br>';
-    echo $cod_banco;
-    echo '<br>';
-    echo $agencia;
-    echo '<br>';
-    echo $valorTed;
-    echo '<br>';
-    echo $numeroConta;
-    echo '<br>'; */
+    
+/* echo $selectConta;
+echo '<br>';
+echo $cod_banco;
+echo '<br>';
+echo $agencia;
+echo '<br>';
+echo $valorTed;
+echo '<br>';
+echo $numeroConta;
+echo '<br>'; */
 
 ?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!--link da font awesome-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!--link do estilo css-->
+    <link rel="stylesheet" href="../assets/css/min.css">
+    <title>Confirmação</title>
+</head>
+<body class="bodyconfirma">
+
+</body>
+</html>
